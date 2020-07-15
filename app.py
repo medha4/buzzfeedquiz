78efd318be1d2aea1694c485d50e96e93d277959
Medha Gupta <medha4@gmail.com>
2020-07-15 18:54:29 +0000

    mvp

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+import random
 
 
 # -- Initialization section --
@@ -13,4 +14,40 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template('index.html')
+
+@app.route('/getCharacter', methods = ['GET','POST'])
+def getCharacter():
+    if request.method == 'GET':
+        return "you are getting"
+    elif request.method == 'POST':
+        props = request.form
+        characters = {
+            'chandler':['sarcastic', 'midlife'],"joey":['funny', 'actor'],"monica":['serious','chef'],
+            'rachel':['airhead','fashion'],
+            'ross':['kind','professor'],
+            'phoebe':['imagine','music']
+        }
+        char_dict = {}
+        for character in characters:
+            common = 0
+            if props['job'] == characters[character][1]:
+                common+=1
+            if props['personality'] == characters[character][0]:
+                common+=1
+            char_dict[character] = common
+        
+
+        like_dict = {}
+        for character in char_dict:
+            if char_dict[character] > 0:
+                like_dict[character] = char_dict[character]
+        
+        if len(like_dict) == 1:
+            return "you are most like: " + next(iter(like_dict))
+        else:
+            return "you are most like: " + random.choice(list(like_dict))
+
+        return like_dict
+    
+
